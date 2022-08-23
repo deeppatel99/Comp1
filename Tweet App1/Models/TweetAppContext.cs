9d736e9e47bf5e5f5998cd9dceee6e6f2bafd2fb
deeppatel99 <deeppatel7521@gmail.com>
2022-08-23 17:41:01 +0530

    file upload

---
+++ Tweet App1/Models/TweetAppContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace com.tweetapp.Models
+{
+    class TweetAppContext : DbContext
+    {
+        public TweetAppContext()
+        {
+
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=LTIN291237;Initial Catalog=TweetAppDb;Integrated Security=true");
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        public DbSet<Tweet> Tweets { get; set; }
+        public DbSet<UserCred> UserCreds { get; set; }
+    }
+}
